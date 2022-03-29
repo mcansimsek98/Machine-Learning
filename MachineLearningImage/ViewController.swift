8715dc378800a5a05024f171669cfa2f604b4d40
Mehmet Can Şimşek <mehmetsimsek>
2022-03-29 11:54:06 +0300

    Initial Commit

---
+++ MachineLearningImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImage
+//
+//  Created by Mehmet Can Şimşek on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
